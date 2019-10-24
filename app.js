0d4b15a0112d9603252bb58408d801fe7ceec7f8
kpancha <kirakpancha@gmail.com>
2019-10-24 18:04:59 -0400

    a lot of things

--- app.js
+++ app.js
@@ -47,4 +47,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.listen(3001, () => console.log("App listening on port 3001!"));
+
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -7,16 +7,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Jeopardy Question Search' });
 });
 
-router.get('/results', function(req, res, next) {
-  res.render('results', { title: 'Search Results' });
-  var options = {
-      min_date: '2014-02-14T01:53:20.027Z',
-      max_date: '2018-05-14T01:53:20.027Z',
-  };
+router.post('/results', function(req, res, next) {
+  const question_value = req.body.question_value;
+  var options = {};
+  if (question_value != 0) {
+    options["value"] = parseInt(question_value);
+    console.log(question_value);
+  }
+  //var options = {
+      //value: parseInt(question_value),
+      //min_date: '2014-02-14T01:53:20.027Z',
+      //max_date: '2018-05-14T01:53:20.027Z',
+  //};
+  const search = req.body.name_field;
   js.clues(options, function(error, response, json){
       if(!error && response.statusCode == 200){
-          console.dir(json);
-          console.log(response.id)
+          //console.dir(json);
+          //console.log(response.id);
+          res.render('results', { title: 'Search Results', results : json })
       } else {
           console.log(`Error: ${response.statusCode}`);
       }
